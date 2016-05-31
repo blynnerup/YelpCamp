caa2423531c7d23ce1e4b967c42acae0e9ffe5a2
Brian Lynnerup <brian.lynnerup.pedersen@gmail.com>
2016-05-31 13:57:40 +0000

    Created the basic routing and some barebones structure to the sites.

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+            {name: "Salmon Creek", image: "https://farm7.staticflickr.com/6184/6098814500_c5aac9703c.jpg"},
+            {name: "Granite Hill", image: "https://farm9.staticflickr.com/8486/8240036928_1a31fbbe9e.jpg"},
+            {name: "Moutain Goats Rest", image: "https://farm9.staticflickr.com/8314/7968774876_11eafbfbb7.jpg"}
+        ];    
+        
+        res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp server has started!"); 
+});
